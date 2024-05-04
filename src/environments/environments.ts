3f695031b138a787245fdec69b83df10fc5efa70
EderGodinez <eder.godinez9511@alumnos.udg.mx>
2024-05-03 23:52:29 -0600

    env updated

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const env={
-  API_KEY:"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",
+  API_KEY:"sk-proj-hkvJc1a7x8oSgO1V994DT3BlbkFJBDYcxJt0ojgP5BXZniGf",
   ChatURL:"https://api.openai.com/v1/chat/completions"
 }
